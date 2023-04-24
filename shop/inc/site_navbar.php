6476cc1ca14d2a0d51fbb6f101cebc3c5ea6fb3e
fares <fares@yahoo.com>
2023-04-24 05:45:39 +0200

    EDIT SLIDER SHOP LOGIN

--- shop/inc/site_navbar.php
+++ shop/inc/site_navbar.php
@@ -113,7 +113,7 @@
                                 <div class="p-3" style="max-width: 700px;">
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3"><?= $row['brand_name'] ?></h4>
                                     <h3 class="display-4 text-white font-weight-semi-bold mb-4"><?= $row['brand_description'] ?></h3>
-                                    <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
+                                    <a href="index.php?page=shop" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
                         </div>
@@ -124,7 +124,7 @@
                                 <div class="p-3" style="max-width: 700px;">
                                     <h4 class="text-light text-uppercase font-weight-medium mb-3"><?= $row['brand_name'] ?></h4>
                                     <h3 class="display-4 text-white font-weight-semi-bold mb-4"><?= $row['brand_description'] ?></h3>
-                                    <a href="" class="btn btn-light py-2 px-3">Shop Now</a>
+                                    <a href="index.php?page=shop" class="btn btn-light py-2 px-3">Shop Now</a>
                                 </div>
                             </div>
                         </div>
